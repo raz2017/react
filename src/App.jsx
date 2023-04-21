6694173bee74dae5d1ee93abfa69ee8612b0d858
Raziel Melchor <razielmelchor@gmail.com>
2023-04-20 22:37:47 -0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import {Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import Details from "./Details.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams/>
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+        </Routes>
+    </BrowserRouter>
+
   );
 };
 
