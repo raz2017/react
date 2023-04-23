679faf57ef6df3bb90e0d2d250c21b73131523aa
Raziel Melchor <razielmelchor@gmail.com>
2023-04-23 00:18:08 -0700

    react features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import Details from "./Details.jsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
 
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <header>
         <Link to="/">Adopt Me!</Link>
       </header>
@@ -13,6 +24,7 @@ const App = () => {
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
         </Routes>
+        </QueryClientProvider>
     </BrowserRouter>
 
   );
